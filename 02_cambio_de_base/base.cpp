117ea2f5fdcde51241382f2fd146a4135d6a80e0
DavidUps <davidarribas.ciclos@gmail.com>
2016-11-21 19:24:13 +0100

    ejemplos c

--- 02_cambio_de_base/base.cpp
+++ 02_cambio_de_base/base.cpp
@@ -6,18 +6,19 @@ int main(){
 
     printf("Decimal: ");
     //escribir por teclado//
+
     scanf(" %i", &decimal);
 
     // Hacer (do)
     //    Imprimir decimal %2
     //    Decimal = decimal / 2
     // Mientras (while) decimal > 0
-
+    printf("numero Binario: ") ;
     do{
       printf("%i", decimal % 2);
       decimal /= 2;
     } while (decimal>0);
-
+    
     printf("\n");
 
     return EXIT_SUCCESS;
