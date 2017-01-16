ed5069f779a2baac32c44c49a11eec36b428c255
DavidUps <davidarribas.ciclos@gmail.com>
2017-01-16 10:30:12 +0100

    ejercicios c

--- 07_ejercicios/caracteres/beep.cpp
+++ 07_ejercicios/caracteres/beep.cpp
@@ -3,11 +3,9 @@
 
 int main(){
 
-    char beep[3] = (54, 54, 0);
-    for(int a=0;a<100; a++)
-        if(beep[a] == beep[3])
-           System.Media.SystemSounds.Beep.Play(1000);
-        else
-            printf("heee");
+    char beep[3] = {54, 54, 0};
+    for(int a=0;a < 3; a++){
+        printf("Caracter: %i\a", beep[a]);
+    }
     return EXIT_SUCCESS;
 }
