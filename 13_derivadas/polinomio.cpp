2beda856361fa3804f23f1107d0abd54ee4152a7
davidups <davidarribas.ciclos@gmail.com>
2017-02-15 19:18:12 +0100

    ejercicios c

--- 13_derivadas/polinomio.cpp
+++ 13_derivadas/polinomio.cpp
@@ -1,27 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int resultado(int x){
 
+    int num[20];
 
-int main(){
-
-    double num[20];
-
-    int num_p, x, resultado=0;
-
-    //numero de terminos del polinomio
-    printf("dime cuantos numeros vas a poner: ");
-    scanf(" %i", &num_p);
+    int resultado=0, potencia[20];
 
     //guarda los numeros
     printf("dime los numeros: ");
-    for(int i=0;i < num_p; i++){
-      scanf(" %lf", &num[i]);
+    for(int i=0;i < x+1; i++){
+      scanf(" %i", &num[i]);
     }
 
-    for(int i=0; i < num_p; i++){
-      printf("%lf", num[i]);
+    //Calcula la portencia
+    for(int grado=0; grado < x; grado++){
+      do{
+          potencia[grado]= x * grado;
+      }while (potencia[x+1]);
     }
+
+    //Multiplica potencia * num
+    for(int i=0; i < x; i++){
+      do{
+          resultado+= potencia[i]*num[i];
+      } while (potencia[i]=potencia[x+1]);
+    }
+
+}
+
+int main(){
+
+    int x;
+
+    printf("dime el valor de x: ");
+    scanf(" %i", &x);
+
+    printf(" %i", resultado(x));
 
 
    return EXIT_SUCCESS;
