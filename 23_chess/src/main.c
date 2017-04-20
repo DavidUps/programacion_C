c9794f1b954b35a3847b8c5acf6f73c0c1f1ade8
Santiago <santiagosieger.ciclos@gmail.com>
2017-04-20 13:34:05 +0200

    chess

---
+++ 23_chess/src/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "general.h"
+#include "io.h"
+
+
+int is_empty (int row, int col, char board[SIZE][SIZE]);
+int tower_check (int row, int col, char board[SIZE][SIZE]);
+
+int
+main (int argc, char *argv[])
+{
+  char chess_board[SIZE][SIZE];
+  int row, col;
+  int num;
+
+  do
+    {
+      load (BOARD_FN, chess_board);
+      dump (chess_board);
+      do
+	{
+	  ask_coordinates (&row, &col, "torre");
+	}
+      while (!is_empty (row, col, chess_board));
+      tower_check (row, col, chess_board);
+    }
+  while (repeat(num));
+
+
+  return EXIT_SUCCESS;
+}
+
+int
+is_empty (int row, int col, char board[SIZE][SIZE])
+{
+  ;
+}
+
+int
+tower_check (int row, int col, char board[SIZE][SIZE])
+{
+  ;
+}
