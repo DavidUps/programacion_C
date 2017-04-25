8c78871b2325ca84253ffd47a87b9657eb207d0c
davidups <davidarribas.ciclos@gmail.com>
2017-04-25 20:23:14 +0200

    chess

--- 23_chess/src/io.c
+++ 23_chess/src/io.c
@@ -1,4 +1,5 @@
 #include "io.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +9,15 @@
 #include <errno.h>
 
 const char *good_chars = "TCAKQP-";
+
+void
+title (const char *s)
+{
+  char command[MAX_CAR];
+  sprintf (command, "toilet -fpagga --gay '    %s    '", s);
+  system ("clear");
+  system (command);
+}
 
 void
 print_usage (FILE * output)
@@ -22,83 +32,122 @@ error_ocurred ()
   error (-errno, errno, "%s\n", strerror (errno));
 }
 
-int
-repeat (void)
-{
-  char valor;
-  int num;
-
-  printf("¿Quieres repetir, s o n?");
-  scanf(" %s", &valor);
-  if(valor== 's')
-      num = 1;
-  else
-      num = 0;
-  return num;
-}
 
 char
 transform (char c)
 {
-   c = toupper (c);
-   if (!strchr(good_chars, c))
-       c = '\0';
-   if (c == '-')
-       c = ' ';
-   return c;
+  c = toupper (c);
+  if (!strchr (good_chars, c))
+    c = '\0';
+  if (c == '-')
+    c = ' ';
+  return c;
 }
 
 void
-
 load (const char *filename, char storage[SIZE][SIZE])
 {
   FILE *pf;
   char c;
   int row = 0, col = 0;
 
-  memset(storage, ' ', SIZE*SIZE);
+  memset (storage, ' ', SIZE * SIZE);
 
   pf = fopen (filename, "r");
   if (!pf)
     error_ocurred ();
 
-  while (!feof (pf) )
-  {
+  while (!feof (pf))
+    {
       c = fgetc (pf);
-      if (c == '\n'){
-          col = 0;
-          row++;
-      }
+      if (c == '\n')
+	{
+	  col = 0;
+	  row++;
+	}
       c = transform (c);
 
       if (c)
-          storage[row][col] = c;
-      col++;
+	{
+	  storage[row][col] = c;
+	  col++;
+	}
 
-  }
+    }
 
   fclose (pf);
 }
 
 void
-dump (char board[SIZE][SIZE])
+prepare_win (int line)
 {
-    int f,c;
-
-    for(f=0; f<SIZE; f++){
-        for(c=0; c<SIZE; c++)
-            printf("%c", board[f][c]);
-        printf("\n");
-    }
+  int i;
+  MOVE (line, 1);
+  for (i = 0; i < 10; i++)
+    printf ("                           \n");
+  MOVE (line, 1);
 }
 
+void
+dump (char board[SIZE][SIZE])
+{
+  grid_show (board);
+}
 
+int
+good_coordinates (int row, int col)
+{
+  return IN_LIMITS(row) && IN_LIMITS(col);
+}
 
 void
 ask_coordinates (int *y, int *x, const char *name)
 {
-    printf("\nDame la fila: ");
-    scanf(" %i", y);
-    printf("Dame la columna: ");
-    scanf(" %i", x);
+  prepare_win (PROMPT_LIN);
+  printf (BOLD_ON "[%s. Posición]" BOLD_OFF "\n", name);
+  ANSI_SWITCH_COLOR (AC_GREEN, FORE_NORMAL);
+  printf ("  fila, columna: ");
+  ANSI_SWITCH_COLOR (AC_GREEN, FORE_LIGHT);
+  scanf ("%i %*[,] %i", y, x);
+  ANSI_COLOR_RESET;
+  (*y)--;
+  (*x)--;
+}
+
+int
+repeat (void)
+{
+  char answer;
+
+  prepare_win (PROMPT_LIN);
+  ANSI_SWITCH_COLOR (AC_GREEN, FORE_NORMAL);
+  printf ("Otra vez (" INVER_ON "s/N" INVER_OFF "): ");
+  scanf (" %c", &answer);
+  ANSI_COLOR_RESET;
+
+  return tolower (answer) == 's' ? 1 : 0;
+}
+
+void
+print_piece (char mark, int row, int col)
+{
+  ANSI (BOLD_ON);
+  ANSI_SWITCH_COLOR (AC_GREEN, BACK_NORMAL);
+  print_number (row, col, mark);
+  ANSI (AC_RESET BOLD_OFF);
+}
+
+void
+print_possibility (int row, int col, char board[SIZE][SIZE])
+{
+  printf ("\tComes => ");
+  ANSI_SWITCH_COLOR (AC_CYAN, BACK_LIGHT);
+  ANSI_SWITCH_COLOR (AC_WHITE, FORE_LIGHT);
+  printf (" (%i, %i):", row + 1, col + 1);
+  ANSI_COLOR_RESET;
+  printf (" ");
+  ANSI_SWITCH_COLOR (AC_CYAN, BACK_LIGHT);
+  ANSI_SWITCH_COLOR (AC_BLUE, FORE_NORMAL);
+  printf (" %c \n", board[row][col]);
+  ANSI_COLOR_RESET;
 }
--- 23_chess/src/main.c
+++ 23_chess/src/main.c
@@ -3,9 +3,8 @@
 
 #include "general.h"
 #include "io.h"
+#include "algorithms.h"
 
-int is_empty (int row, int col, char board[SIZE][SIZE]);
-int tower_check (int row, int col, char board[SIZE][SIZE]);
 
 int
 main (int argc, char *argv[])
@@ -16,45 +15,30 @@ main (int argc, char *argv[])
   do
     {
       load (BOARD_FN, chess_board);
+      title ("chess");
       dump (chess_board);
       do
 	{
-	  ask_coordinates (&row, &col, "torre");
+	  ask_coordinates (&row, &col, "Torre");
 	}
-      while (!is_empty (row, col, chess_board));
+      while (!good_coordinates (row, col) ||
+	     !is_empty (row, col, chess_board));
+      print_piece ('T', row, col);
       tower_check (row, col, chess_board);
-    }
-  while (repeat());
+      do
+	{
+	  ask_coordinates (&row, &col, "ALfil");
+	}
+      while (!good_coordinates (row, col) ||
+	     !is_empty (row, col, chess_board));
+      print_piece ('A', row, col);
+      alfil_check (row, col, chess_board);
 
+    }
+  while (repeat ());
+
+  MOVE(EXIT_LIN, 1);
 
   return EXIT_SUCCESS;
 }
 
-int
-is_empty (int row, int col, char board[SIZE][SIZE])
-{
-    if(board[row][col]==' '){
-       printf("Puedes Ponerla");
-    }
-    printf("Esta ocupada");
-  ;
-}
-
-int
-tower_check (int row, int col, char board[SIZE][SIZE])
-{
-      int f,c;
-      int retorno;
-      printf("%i", col);
-      printf("%i", row);
-      for (c=0; c<SIZE;c++){
-          if(board[f][c]!=' ')
-              retorno=1;
-      }
-      for(f=0; f<SIZE;f++){
-          if(board[f][c]!=' ')
-              retorno=1;
-      }
-      if(retorno=1)
-          printf("Se Come Una Ficha\n");
-}
