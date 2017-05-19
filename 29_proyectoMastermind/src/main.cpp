65dd34aa7ef7f601acdbd62331a993e9a08bb226
davidups <davidarribas.ciclos@gmail.com>
2017-05-19 14:27:49 +0200

    mastermind

---
+++ 29_proyectoMastermind/src/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    char chess[4][10];
+    char resul[4];
+    char check[2];
+
+    printcheck(chess);
+    printresul(resul);
+    isnertresul(resul);
+    for(int i=0;i>11;i++){
+        for(i>11){
+            insetchess(chess);
+            printcheck(chess);
+        }while(false);
+    }
+
+    return EXIT_SUCCESS;
+}
